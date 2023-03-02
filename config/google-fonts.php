1b1b127473bbdebf5e1946defbaeb8742e666510
NatanFiuza <natan.fiuza@gmail.com>
2023-03-02 06:28:29 -0300

    Atualizações de teste do layout

---
+++ config/google-fonts.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'fonts' => [
+        //'default' => 'https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+        'default' => 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap',
+    ],
+];
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
-     <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+     <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
